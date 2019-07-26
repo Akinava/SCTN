8e26ecd193ea19ffb850fc49886a3be89024d573
Akinava <akinava@gmail.com>
2019-07-26 10:02:29 +0200

    init

---
+++ src/host.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Akinava'
+__author_email__ = 'akinava@gmail.com'
+__copyright__ = "Copyright © 2019"
+__license__ = "MIT License"
+__version__ = [0, 0]
+
+
+class UDPHost:
+    def __init__(self, Handler):
+        pass
+
+    def start(self):
+        while(True):
+            pass
+
+    def stop(self):
+        pass
---
+++ test/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+__author__ = 'Akinava'
+__author_email__ = 'akinava@gmail.com'
+__copyright__ = "Copyright © 2019"
+__license__ = "MIT License"
+__version__ = [0, 0]
+
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(test_dir, '../src')
+sys.path.append(src_dir)
+import host
+
+
+class Handler:
+    pass
+
+
+if __name__ == "__main__":
+    print('start test')
+    server = host.UDPHost(Handler)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+    print('end test')
