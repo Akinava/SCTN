07b23bb9923bf7cc139a948b897e8c5492298e24
Andrei Chernov <Andrei.Chernov1@thomsonreuters.com>
2021-02-23 20:45:36 +0100

    fix listener

--- src/host.py
+++ src/host.py
@@ -17,41 +17,34 @@ class UDPHost:
         logger.info('UDPHost init')
         self.handler = handler
         self.crypt_tools = crypt_tools.Tools()
-        self.listeners = {}
         self.connections = {}
 
     def connect(self, host, port):
         logger.info('host connect to {} {}'.format(host, port))
 
     async def create_listener(self, port):
-        if self.check_port_busy(port):
-            return
         logger.info('UDPHost create_listener')
         loop = asyncio.get_running_loop()
         logger.info('host create_listener on port {}'.format(port))
         transport, protocol = await loop.create_datagram_endpoint(
             lambda: self.handler(),
             local_addr=('0.0.0.0', port))
-        self.listeners[port] = {
+        self.listener = {
+            'port': port,
             'transport': transport,
             'protocol': protocol,
         }
 
-    def check_port_busy(self, port):
-        return port in self.listeners
-
     async def serve_forever(self):
         logger.info('UDPHost serve_forever')
-        while self.listeners or self.connections:
+        while self.listener or self.connections:
             await asyncio.sleep(settings.time_check)
 
-    def shutdown_listener(self, port):
-        if not port in self.listeners:
-            return
-        self.listeners[port]['transport'].close()
-        del self.listeners[port]
+    def shutdown_listener(self):
+        self.listener['transport'].close()
+        self.listener = {}
 
     def __del__(self):
         logger.info('UDPHost __del__')
         #for port in self.listeners:
-        #    self.shutdown_listener(port)
+        #    self.shutdown_listener()
